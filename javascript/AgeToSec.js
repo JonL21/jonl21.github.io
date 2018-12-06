f16134a9850cb458be584c7ae2c2d5d378161a30
JonL21 <jlorayes@users.noreply.github.com>
2018-12-06 11:45:25 -0500

    Age To Seconds Functionality

--- javascript/AgeToSec.js
+++ javascript/AgeToSec.js
@@ -1,4 +1,4 @@
 function AgeToSec() {
     const age = document.getElementById('age').value
-    document.getElementById('output').innerHTML = age * 60 * 60 * 24 * 365.2425
+    document.getElementById('output').innerHTML = `You are ${age * 60 * 60 * 24 * 365.2425} seconds old!`
 }
